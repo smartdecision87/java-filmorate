1468aeb3934d504dc6c86236d1a8e10413c0b90f
Vladislav <petrvladvic@yandex.ru>
2025-10-14 06:41:54 +0300

    fix codestyle 5

--- src/test/java/ru/yandex/practicum/filmorate/FilmTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import ru.yandex.practicum.filmorate.controller.FilmController;
-import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
